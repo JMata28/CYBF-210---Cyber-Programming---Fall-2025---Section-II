41f1d52ff1017a323ae95ae853ecb036ec77dd8a
Prof Mata <profmata@Joses-MacBook-Pro.local>
2025-11-19 22:54:04 -0500

    Updated content for Week 12 on 11.19.2025

--- Week 11/max_heaps.py
+++ Week 11/max_heaps.py
@@ -5,19 +5,25 @@
 import heapq  
 nums = [10, 20, 15, 30, 40]  
 # Convert into a max-heap by inverting values  
-max_heap = [-n for n in nums]  
-heapq.heapify(max_heap)  
-# Access largest element (invert sign again)  
-print("Largest element:", -max_heap[0])
+temp_heap = [-n for n in nums]  #temp_heap is the max_heap but with negative values, so it still is not our final max_heap
+heapq.heapify(temp_heap)  
+print("This is our temporary heap: ", temp_heap)
+max_heap = [-n for n in temp_heap]
+# Access largest element  
+print("Largest element:", max_heap[0])
 
 #ADDING NEW ELEMENTS (7, 50, and 2)
-heapq.heappush(max_heap, -7) #Notice how the values added must be negated
-heapq.heappush(max_heap, -50)
-heapq.heappush(max_heap, -2)
+heapq.heappush(temp_heap, -7) #Notice how the values added must be negated because I'm adding them to temp_heap
+heapq.heappush(temp_heap, -50)
+heapq.heappush(temp_heap, -2)
+print("This is our temporary after adding -7, -50, and -2: ", temp_heap)
+max_heap = [-n for n in temp_heap]
+print("This is our max heap after adding 7, 50, and 2: ", max_heap)
 
 # ACCESS OR REMOVE THE LARGEST ELEMENT
-largest = -heapq.heappop(max_heap)
+largest = -heapq.heappop(temp_heap)
 print("Removed largest element:", largest)
-print("Heap after removal:", [-x for x in max_heap])
+max_heap = [-n for n in temp_heap]
+print("Max Heap after removal:", max_heap)
 
 #And so on...
